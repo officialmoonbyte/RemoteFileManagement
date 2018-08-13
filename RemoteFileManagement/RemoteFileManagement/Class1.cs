c542813213157d9123326c32d5ad110943cf9dda
Alexander Ritter <braydelritter@gmail.com>
2018-08-13 17:51:25 -0500

    First Commit

---
+++ RemoteFileManagement/RemoteFileManagement/Class1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using IndieGoat.UniversalServer.Interfaces;
+
+namespace RemoteFileManagement
+{
+    public class RemoteFileManagement : IServerPlugin
+    {
+        public string Name
+        {
+            get { return "RemoteFileManagement"; }
+        }
+
+        public event EventHandler<SendMessageEventArgs> SendMessage;
+
+        #region Vars
+
+        int Port = 9984;
+
+        #endregion
+
+        #region OnLoad
+
+        public void onLoad(string ServerDirectory)
+        {
+            string PluginDirectory = ServerDirectory + @"\RemoteFileManagement";
+            string FileDirectory = PluginDirectory + @"\Port.txt";
+
+            if (File.Exists(FileDirectory))
+            {
+                string sPort = File.ReadAllText(FileDirectory);
+                Port = int.Parse(sPort);
+            }
+            else
+            {
+                File.Create(FileDirectory).Close();
+                File.WriteAllText(FileDirectory, Port.ToString());
+            }
+        }
+
+        #endregion
+
+        #region OnExit
+
+        public void Unload() { }
+
+        #endregion
+
+        #region Invoke
+
+        public void Invoke(ClientSocketWorkload workload, ClientContext context, int port, List<string> Args, string ServerDirectory)
+        {
+            string Command = Args[1].ToUpper();
+
+            if (Command == "FILETOSERVER") { }
+            if (Command == "FILETOCLIENT") { }
+            if (Command == "GETDIRECTORIES") { }
+            if (Command == "GETFILES") { }
+            if (Command == "DELETEFILE") { }
+            if (Command == "EDITFILE") { }
+            if (Command == "COPYFILE") { }
+        }
+
+        #endregion
+
+        #region Commands
+
+        #region FileToServer
+
+
+
+        #endregion
+
+        #region FileToClient
+
+
+
+        #endregion
+
+        #endregion
+
+    }
+}
---
+++ RemoteFileManagement/RemoteFileManagement/TcpServer.cs
@@ -0,0 +1,49 @@
+﻿
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace RemoteFileManagement
+{
+    public class TcpServer
+    {
+        public EventHandler<EventArgs> ReceivedMessage;
+
+        public TcpServer(int Port)
+        {
+            try
+            {
+                TcpListener listener = new TcpListener(IPAddress.Any, Port);
+                listener.Start();
+
+                while (true)
+                {
+                    TcpClient client = listener.AcceptTcpClient();
+                    new ClientHandler(this, client);
+                }
+            }
+            catch (Exception ex) { Console.WriteLine("Could not start up RemoteFileManagement : " + ex.Message); }
+        }
+    }
+
+    public class ClientHandler
+    {
+        Byte[] bytes = new byte[1024];
+        string Data = null;
+
+        TcpServer Server;
+        TcpClient Client;
+
+        public ClientHandler(TcpServer server, TcpClient client)
+        {
+            Server = server;
+            Client = client;
+
+            Thread thread = new Thread(new ThreadStart(() =>
+            {
+
+            })); thread.Start();
+        }
+    }
+}
